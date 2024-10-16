09dc9b43d2b407bd1ffff6360efb2cd9a01d6eff
Maximilian McClelland <mwmttl@gmail.com>
2024-10-16 11:46:49 -0600

    Getting Started

---
+++ MessagingApp/MessagingApp/MessagingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessagingAppApp.swift
+//  MessagingApp
+//
+//  Created by Maximilian McClelland on 10/16/24.
+//
+
+import SwiftUI
+
+@main
+struct MessagingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MessagingApp/MessagingAppTests/MessagingAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  MessagingAppTests.swift
+//  MessagingAppTests
+//
+//  Created by Maximilian McClelland on 10/16/24.
+//
+
+import Testing
+@testable import MessagingApp
+
+struct MessagingAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
